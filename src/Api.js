8dddb1e9857c0f92a832bc1588ddb8f169fe6b8b
abbasmurudkar <abbasmurudkar18@icloud.com>
2022-03-02 19:13:26 +0530

    RAZORPAY_INTERGRATION

--- src/Api.js
+++ src/Api.js
@@ -1,11 +1,59 @@
 import React, { useEffect, useState } from 'react'
 import styled from 'styled-components';
 import Spinner from './Spinner';
+import image from "./logo.png"
 function Api() {
   const [data, setcategories] = useState([]);
   const [loading, setloading] = useState(true)
 
   const price = 500
+  const order_id = [
+{
+  order_id: 1,
+},
+{
+  order_id: 2,
+},
+{
+  order_id: 3,
+},
+{
+  order_id: 4,
+},
+{
+  order_id: 5,
+},
+{
+  order_id: 6,
+},
+{
+  order_id: 7,
+},
+{
+  order_id: 8,
+},
+{
+  order_id: 9,
+},
+{
+  order_id: 10,
+},
+{
+  order_id: 11,
+},
+{
+  order_id: 12,
+},
+{
+  order_id: 13,
+},
+{
+  order_id: 14,
+},
+  ]
+
+    
+  
   var formatter = new Intl.NumberFormat("en-US",{
     style: "currency",
     currency:"INR"
@@ -41,7 +89,7 @@ function Api() {
   }
   const displayrazorpay = async (amount) =>{
   const res = await loadScript('https://checkout.razorpay.com/v1/checkout.js')
-
+console.log(res)
   if(!res){
     alert("your are offlibe failed to load razorpay")
   return;
@@ -51,16 +99,20 @@ function Api() {
     key: "rzp_test_mNj333YrVgR9NC",
     currency: "INR",
     amount:amount *100,
-    name: "mohd abbas",
+    name: "BROOZ MEALS",
     description: "THANKS FOR PURCHASING",
-
+    image :`${image}`,
     handler: function (response) {
       alert(response.razorpay_payment_id)
       alert("payment successful")
     },
     prefill:{
       name: "ABBAS MURUDKAR"
-    }
+    },
+    theme: {
+      color: "#3399cc",
+      // backdrop_color:"#4455cc"
+  }
   };
   const paymentOnject = new window.Razorpay(options)
   paymentOnject.open()
@@ -75,12 +127,12 @@ function Api() {
           <div className='info'>
             <img src={r.strCategoryThumb} alt="loading" />
             <div className='vl'></div>
-        
             <p><h3>{r.strCategory}</h3><br/>{r.strCategoryDescription ? r.strCategoryDescription.slice(0, 350) : ""}..<br/><br/><span style={{color:"green", fontWeight:"bolder"}}>{formatter.format(price)}</span></p> 
             <button onClick={()=> displayrazorpay(price)}>Order Now</button>
           </div>
         </div>
       ))}
+
     </Cover>
   )
 }
